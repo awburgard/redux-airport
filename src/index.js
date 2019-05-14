30963d064578c210a43f357b25aac0b7366cfb03
Austin Burgard <awburgard@gmail.com>
2019-05-14 13:37:02 -0500

    adds airplaneReducer and links in Provider to App

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers} from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const airplaneReducer = (state = [], action) =>{
+    console.log(`I'm working!`)
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        airplaneReducer,
+    })
+)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider> , document.getElementById('root'));
